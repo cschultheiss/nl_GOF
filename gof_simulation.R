f8ffeb3d49b7f3913244fe194f7a71141e713435
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2023-03-16 13:29:42 +0100

    Steps.in vs steps.out

--- gof_simulation.R
+++ gof_simulation.R
@@ -43,7 +43,7 @@ Vx <- function(x2) 1 - (up(x2) * dnorm(up(x2)) - lo(x2) * dnorm(lo(x2)))/(pnorm(
 
 nsim <- 200
 n.vec <- 10^(2:5)
-n.split <- 50
+n.split <- 25
 p <- 2
 b <- 1.5
 a <- sqrt(1/3)
@@ -84,24 +84,38 @@ for (n in n.vec) {
                  
                  mse <- mean((fi.all$residuals - (x0 - Ex(x1)))^2)
                  rcor <- cor(fi.all$residuals, x0 - Ex(x1), method = "spearman")
-                 
-                 sel <- matrix(NA, n.split, dim(dat)[2] - 1)
-                 steps <- matrix(NA, nrow = n.split, ncol = dim(dat)[2] - 1)
+                 sel11 <- sel12 <- sel21 <- sel22 <- matrix(NA, n.split, dim(dat)[2] - 1)
+                 steps11 <-  matrix(NA, nrow = n.split, ncol = dim(dat)[2] - 1)
+                 colnames(steps11) <- colnames(dat)[-1]
+                 steps12 <- steps21 <- steps22 <- steps11
                  for(i in 1:n.split){
                    ind <- sample(n, n/2)
-                   fi <- gam(y ~ s(x1) + s(x2), data = dat[-ind,])
-                   pred <- predict(fi, newdata = dat[ind,])
-                   fo <- foci(abs(y[ind] - pred), dat[ind, -1])
-                   steps[i, fo$selectedVar$index] <- diff(c(0, fo$stepT))
-                   nsel <- length(fo$selectedVar$index)
-                   sel[i,] <- c(fo$selectedVar$index, rep(NA, dim(dat)[2] - 1 - nsel))
+                   fi1 <- gam(y ~ s(x1) + s(x2), data = dat[ind,])
+                   fi2 <- gam(y ~ s(x1) + s(x2), data = dat[-ind,])
+                   pred11 <- predict(fi1, newdata = dat[ind,])
+                   pred12 <- predict(fi1, newdata = dat[-ind,])
+                   pred21 <- predict(fi2, newdata = dat[ind,])
+                   pred22 <- predict(fi2, newdata = dat[-ind,])
+                   fo11 <- foci(abs(y[ind] - pred11), dat[ind, -1])
+                   fo12 <- foci(abs(y[-ind] - pred12), dat[-ind, -1])
+                   fo21 <- foci(abs(y[ind] - pred21), dat[ind, -1])
+                   fo22 <- foci(abs(y[-ind] - pred22), dat[-ind, -1])
+                   steps11[i, fo11$selectedVar$index] <- diff(c(0, fo11$stepT))
+                   steps12[i, fo12$selectedVar$index] <- diff(c(0, fo12$stepT))
+                   steps21[i, fo21$selectedVar$index] <- diff(c(0, fo21$stepT))
+                   steps22[i, fo22$selectedVar$index] <- diff(c(0, fo22$stepT))
+                   sel11[i,] <- c(fo11$selectedVar$index, rep(NA, dim(dat)[2] - 1 - length(fo11$selectedVar$index)))
+                   sel12[i,] <- c(fo12$selectedVar$index, rep(NA, dim(dat)[2] - 1 - length(fo12$selectedVar$index)))
+                   sel21[i,] <- c(fo21$selectedVar$index, rep(NA, dim(dat)[2] - 1 - length(fo21$selectedVar$index)))
+                   sel22[i,] <- c(fo22$selectedVar$index, rep(NA, dim(dat)[2] - 1 - length(fo22$selectedVar$index)))
                  }
                  
                  out <- list()
                  out$values <- c(mse, rcor, sel.all0, sel.all)
-                 colnames(steps) <- colnames(dat)[-1]
-                 out$steps <- steps
-                 out$sel <- sel
+                 out$steps.in <- rbind(steps11, steps22)
+                 out$steps.out <- rbind(steps21, steps12)
+                 out$sel.in <- rbind(sel11, sel22)
+                 out$sel.out <- rbind(sel21, sel12)
                  
                  out
                }
@@ -113,20 +127,23 @@ for (n in n.vec) {
   colnames(res.val) <- c("mse", "rcor",
                          paste(rep(c("all0", "all"), each = 2), rep(c("x1", "x2")), sep ="."))
   
-  res.steps <- array(unlist(res[,"steps"]), dim = c(n.split, p, nsim), dimnames = list(NULL,
-                                                                                 colnames(res[1,"steps"][[1]]), NULL))
-  res.sel <- array(unlist(res[,"sel"]), dim = c(n.split, p, nsim))
+  res.steps.in <- array(unlist(res[,"steps.in"]), dim = c(2 * n.split, p, nsim), dimnames = list(NULL,
+                                                                                 colnames(res[1,"steps.in"][[1]]), NULL))
+  res.steps.out <- array(unlist(res[,"steps.out"]), dim = c(2 * n.split, p, nsim), dimnames = list(NULL,
+                                                                                                 colnames(res[1,"steps.out"][[1]]), NULL))
+  res.sel.in <- array(unlist(res[,"sel.in"]), dim = c(2 * n.split, p, nsim))
+  res.sel.out <- array(unlist(res[,"sel.out"]), dim = c(2 * n.split, p, nsim))
   
   # store output quantities, sample size, random seed, commit
-  simulation <- list(all = res.val, steps = res.steps, sel = res.sel,
-                     n = n, r.seed = attr(res, "rng"), "commit" = commit)
+  simulation <- list(all = res.val, steps.in = res.steps.in, steps.out = res.steps.out, sel.in = res.sel.in,
+                     sel.out = res.sel.out, n = n, r.seed = attr(res, "rng"), "commit" = commit)
   # create unique filename based on sample size and time
   resname <- paste0("results n=", n, " ", format(Sys.time(), "%d-%b-%Y %H.%M"))
   # save the file to the folder
   if (save) save(simulation, file = paste("results/", newdir, "/", resname, ".RData", sep = ""))
   print(apply(res.val, 2, mean))
   for (j in 1:p){
-    print(paste(j, ": ", sum(res.sel == j, na.rm = TRUE), sep = ""))
+    print(paste(j, ": ", sum(res.sel.in == j, na.rm = TRUE), sep = ""))
+    print(paste(j, ": ", sum(res.sel.out == j, na.rm = TRUE), sep = ""))
   }
-
 }
