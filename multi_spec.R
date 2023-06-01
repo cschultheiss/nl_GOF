205dcdca9d4e08bfd2f7b2816afb0f4cf1d0c928
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2023-06-01 11:35:26 +0200

    return residuals, generic transform

--- multi_spec.R
+++ multi_spec.R
@@ -1,8 +1,7 @@
 multi.spec <- function(data, response = "y", B = 25, gamma = NULL, gamma.min = 0.05,
                        fitting = function(data, ind) gam(wrapFormula(y ~., data = data), data = data[ind, ]),
                        predicting = function(fit, data, ind) predict(fit, newdata = data[ind,]),
-                       norming = NULL,
-                       return.predictor = FALSE){
+                       norming = NULL, trafo = abs, return.predictor = FALSE, return.residual = FALSE){
   if(is.null(gamma)){
     gamma <- (1 : (2 * B))/(2 * B)
     gamma <- gamma[gamma >= gamma.min]
@@ -10,7 +9,7 @@ multi.spec <- function(data, response = "y", B = 25, gamma = NULL, gamma.min = 0
   n <- nrow(data)
   res.ind <- which(colnames(data) == response)
   pval <- numeric(2 * B)
-  pred <- matrix(0, n, B)
+  res <- pred <- matrix(0, n, B)
   sel12 <- sel21 <- matrix(NA, B, dim(data)[2] - 1)
   steps12 <-  matrix(NA, nrow = B, ncol = dim(data)[2] - 1)
   colnames(steps12) <- colnames(data)[-res.ind]
@@ -35,6 +34,9 @@ multi.spec <- function(data, response = "y", B = 25, gamma = NULL, gamma.min = 0
       res21  <- res21 / norm21
     }
     
+    res[ind, i] <- res21
+    res[-ind, i] <- res12
+    
     if (n /2 > 1e4){
       hs12 <- dhsic.test((res12)[1:1e4], data[-ind, -res.ind][1:1e4 ,], method = "gamma")
       hs21 <- dhsic.test((res21)[1:1e4], data[ind, -res.ind][1:1e4 ,], method = "gamma")
@@ -45,8 +47,8 @@ multi.spec <- function(data, response = "y", B = 25, gamma = NULL, gamma.min = 0
     
     pval[c(i, i + B)] <- c(hs12$p.value, hs21$p.value)
     
-    fo12 <- foci(abs(res12), data[-ind, -res.ind])
-    fo21 <- foci(abs(res21), data[ind, -res.ind])
+    fo12 <- foci(trafo(res12), data[-ind, -res.ind])
+    fo21 <- foci(trafo(res21), data[ind, -res.ind])
     steps12[i, fo12$selectedVar$index] <- diff(c(0, fo12$stepT))
     steps21[i, fo21$selectedVar$index] <- diff(c(0, fo21$stepT))
     sel12[i,] <- c(fo12$selectedVar$index, rep(NA, dim(data)[2] - 1 - length(fo12$selectedVar$index)))
@@ -62,5 +64,6 @@ multi.spec <- function(data, response = "y", B = 25, gamma = NULL, gamma.min = 0
   out <- list(pval.corr = pval.pre, pval = pval,
               steps = rbind(steps21, steps12), sel = rbind(sel21, sel12))
   if (return.predictor) out$prediction <- pred
+  if (return.residual) out$residual <- res
   return(out)
 }
