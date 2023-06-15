d3d4b4f81a32371079b4074ec6ea2ce29b9704d2
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2023-06-15 11:32:41 +0200

    Analyse H0 cases separately

--- figures_multi.R
+++ figures_multi.R
@@ -35,9 +35,12 @@ stab.col <- rep(FALSE, prod(dim(all.comb)))
 for (s in 1:ncol(all.comb)){
   mes <- all.comb[, s]
   stab.col[(s - 1) * p.mes + which(mes %in% which.stab(mes))] <- TRUE
+  if(length(which.stab(mes)) == length(mes))
+    stab.col[(s - 1) * p.mes + 1:3] <- NA
+    
 }
-stab <- (1:prod(dim(all.comb)))[stab.col]
-unstab <- (1:prod(dim(all.comb)))[-stab]
+stab <- which(stab.col)
+unstab <- which(!stab.col)
 
 get.pval <- TRUE
 
@@ -152,4 +155,59 @@ for (file in flz){
   
   lines(c(sort(pv.corr.stab), 1), (1:(npc + 1))/(npc + 1), col = cols[t], lty = ltys[t], lwd = exp.lines)
   points(c(sort(pv.corr.stab), 1), (1:(npc + 1))/(npc + 1), col = alpha(cols[t], 0.2))
+}
+
+
+exp.text <- 1.5
+exp.points <- 1.5
+exp.lines <- 1.5
+cols <- ltys <-  1:4
+pchs <- c(0:2, 6)
+get.pval <- FALSE
+t <- 0
+for (file in flz){
+  t <- t + 1
+  load(paste(folder, "/", file, sep = ""))
+  pv.all <- matrix(NA, ncol = 100, nrow = 0)
+  all.s.all <- array(NA, dim = c(50, 0, 100))
+  for (s in 1:ncol(all.comb)){
+    mes <- all.comb[, s]
+    if(length(which.stab(mes)) < length(mes)) next()
+    all.s <- simulation[[paste(mes, collapse = "")]]$steps.out
+    B <- dim(all.s)[1]
+    if (get.pval){
+      pval.lim <- 0.05
+      glob <- which(simulation[[paste(mes, collapse = "")]]$pval.corr > pval.lim)
+      glob.s <- t(simulation[[paste(mes, collapse = "")]]$pval[,c((B/2 + 1) : B, 1 : (B/2))]) > pval.lim
+    }
+    pv <- apply(1 * is.na(all.s), 3, fisher.split)
+    if(get.pval) pv[, glob] <- 0
+    pv.all <- rbind(pv.all, pv)
+    
+    all.s <- is.na(all.s)
+    if (get.pval){
+      all.s <- aperm(all.s, c(1, 3, 2))
+      all.s <- pmax(all.s, glob.s)
+      all.s <- aperm(all.s, c(1, 3, 2))
+    }
+    
+    all.s.all <- abind(all.s.all, all.s, along =  2)
+  }
+  
+  pis <- sort(unique(c(as.matrix(pv.all))))
+  pis <- pis[pis < 1]
+  
+
+  r2 <- sapply(pis, function(pi) mean(pv.all <= pi))
+  
+  if (t == 1) {
+    plot(pis, r2, xlim = c(0,max(pis)), ylim = c(0,1), type = "l", col = cols[t], lty = ltys[t],
+         xlab = "Threshold", ylab = "True positive rate", cex.lab = exp.text, lwd = exp.lines)
+  } else {
+    points(pis, r2, xlim = c(0,1), ylim = c(0,1), type = "l", col = cols[t], lty = ltys[t], lwd = exp.lines)
+  }
+  pi0 <- 0.01
+  points(pi0, mean(pv.all <= pi0), pch = 4, col = cols[t], cex = exp.points)
+  
+  points(0.5, mean(all.s.all), pch = pchs[t], col = cols[t], cex = exp.points)
 }
