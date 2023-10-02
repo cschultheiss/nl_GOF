ae9ce1091e2ad1911b79ec7d11c9649410a8457f
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2023-10-02 17:13:33 +0200

    All data

--- k562_pc.R
+++ k562_pc.R
@@ -28,9 +28,9 @@ dat <- mat[["expression_matrix"]][obs, sel.col]
 colnames(dat) <- sel.var
 
 tic()
-algo <- pc_parallel(suffStat = list(data=dat[,1:5], ic.method="hsic.gamma"),
+algo <- pc_parallel(suffStat = list(data=dat, ic.method="hsic.gamma"),
    indepTest = kernelCItest, alpha = 1e-3, m.max = 2,
-   labels = sel.var[1:5], verbose = TRUE, num.cores = 5)
+   labels = sel.var, verbose = TRUE, num.cores = 5)
 toc()
 
 save <- TRUE
