064ac527a88351c1bf9d78a321e9666c06ac8381
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2023-10-18 17:46:18 +0200

    Analyse and print several y:
    To do: consistent number formatting

--- k562_pc.R
+++ k562_pc.R
@@ -88,74 +88,86 @@ for(y in 1:28){
   subs <- sort(c(which(ad[y,] > 1),y))
   
   test.sub <- test[subs, subs]
-  if(length(subs) > 1)
+  if(length(subs) > 2 && any(test.sub[subs == y, subs != y] > 0.01)){
     cat(y, ": ", test.sub[subs == y, subs != y], "\n")
-}
-
-y <- 16 # 6, 9 also work "reasonable"
-subs <- sort(c(which(ad[y,] > 1),y))
-
-test.sub <- test[subs, subs]
-
-diag(test.sub) <- 1
-
-
-dat.reg <- dat[,subs]
-wi <- which(colnames(dat.reg) == sel.var[y])
-dat.reg <- cbind(dat.reg[,wi], dat.reg[,-wi])
-colnames(dat.reg) <- c("y", colnames(dat[,subs])[-wi])
-
-set.seed(4)
-msx <- multi.spec(data.frame(dat.reg), B = 25, return.predictor = TRUE, return.residual = TRUE,
-  fitting = function(data, ind) fitxg(data, ind, ncol(data) - 1), predicting = predxg, parallel = TRUE, sockets = 5)
-apply(is.na(msx$steps), 2, sum)
-fisher.split(is.na(msx$steps))
-mean(msx$residual^2)
-var(c(msx$residual))
-
-msg <- multi.spec(data.frame(dat.reg), B = 25, return.predictor = TRUE, return.residual = TRUE,
-                  parallel = TRUE, sockets = 5)
-apply(is.na(msg$steps), 2, sum)
-fisher.split(is.na(msg$steps))
-mean(msg$residual^2)
-var(c(msg$residual))
-
-
-data.x <- xgb.DMatrix(dat.reg[,-1], label = dat.reg[,1])
-fi.all <- xgb.cv(list(max_depth = ncol(dat.reg) - 1, nthread = 1), data = data.x, nrounds = 500, nfold = 2,
-                 early_stopping_rounds = 3, prediction = TRUE, verbose = TRUE)
-
-
-
-env.data <- list()
-for (su in subs){
-  rows <- which(mat["interventions"] == sel.var[su])
-  dati <- mat[["expression_matrix"]][rows, sel.col[subs]]
-  env.data[[sel.var[su]]] <- dati
-}
-
-watchlist <- env.data
-for (wa in names(watchlist)){
-  watchlist[[wa]] <- xgb.DMatrix(watchlist[[wa]][,-which(subs == y)], label = watchlist[[wa]][,which(subs == y)])
-}
-fi.out <- xgb.train(list(max_depth = ncol(dat.reg) - 1, nthread = 1), data = data.x, nrounds = fi.all$best_iteration,
-                    verbose = TRUE, watchlist = watchlist)
-
-par(mfrow = c(3,1))
-for(wa in names(env.data)){
-  if (wa == sel.var[y])
     next()
-  pred <- predict(fi.out, watchlist[[wa]])
-  print(mean((pred - env.data[[wa]][,which(subs == y)])))
 
-  # qqplot(dat[,y], env.data[[wa]][,which(subs == y)])
-  # abline(0,1)
-  # qqplot(dat[,which(sel.var == wa)], env.data[[sel.var[y]]][,which(sel.var[subs] == wa)])
-  # abline(0,1)
-  plot(dat[,which(sel.var == wa)], dat[,y], xlim = c(0, 6), ylim = c(0, 6),
-       xlab = wa, ylab = sel.var[y])
-  points(env.data[[wa]][,which(sel.var[subs] == wa)],  env.data[[wa]][,which(subs == y)], col = 2)
-  # points(env.data[[sel.var[y]]][,which(sel.var[subs] == wa)],  env.data[[sel.var[y]]][,which(subs == y)], col = 3)
-  points(env.data[[wa]][,which(sel.var[subs] == wa)], pred, col = 3)
+
+    #y <- 16 # 6, 9 also work "reasonable"
+    subs <- sort(c(which(ad[y,] > 1),y))
+    
+    test.sub <- test[subs, subs]
+    
+    diag(test.sub) <- 1
+    
+    
+    dat.reg <- dat[,subs]
+    wi <- which(colnames(dat.reg) == sel.var[y])
+    dat.reg <- cbind(dat.reg[,wi], dat.reg[,-wi])
+    colnames(dat.reg) <- c("y", colnames(dat[,subs])[-wi])
+    
+    set.seed(4)
+    msx <- multi.spec(data.frame(dat.reg), B = 25, return.predictor = TRUE, return.residual = TRUE,
+      fitting = function(data, ind) fitxg(data, ind, ncol(data) - 1), predicting = predxg, parallel = TRUE, sockets = 5)
+    nsplit <- apply(is.na(msx$steps), 2, sum)
+    fisher.split(is.na(msx$steps))
+    pval <- test.sub[subs == y, subs != y]
+    mean(msx$residual^2)
+    var(c(msx$residual))
+    
+    # msg <- multi.spec(data.frame(dat.reg), B = 25, return.predictor = TRUE, return.residual = TRUE,
+    #                   parallel = TRUE, sockets = 5)
+    # apply(is.na(msg$steps), 2, sum)
+    # fisher.split(is.na(msg$steps))
+    # mean(msg$residual^2)
+    # var(c(msg$residual))
+    
+    
+    data.x <- xgb.DMatrix(dat.reg[,-1], label = dat.reg[,1])
+    fi.all <- xgb.cv(list(max_depth = ncol(dat.reg) - 1, nthread = 1), data = data.x, nrounds = 500, nfold = 2,
+                     early_stopping_rounds = 3, prediction = TRUE, verbose = FALSE)
+    
+    
+    
+    env.data <- list()
+    for (su in subs){
+      rows <- which(mat["interventions"] == sel.var[su])
+      dati <- mat[["expression_matrix"]][rows, sel.col[subs]]
+      env.data[[sel.var[su]]] <- dati
+    }
+    
+    watchlist <- env.data
+    for (wa in names(watchlist)){
+      watchlist[[wa]] <- xgb.DMatrix(watchlist[[wa]][,-which(subs == y)], label = watchlist[[wa]][,which(subs == y)])
+    }
+    fi.out <- xgb.train(list(max_depth = ncol(dat.reg) - 1, nthread = 1), data = data.x, nrounds = fi.all$best_iteration,
+                        verbose = FALSE, watchlist = watchlist)
+    
+    par(mfrow = c(3,1))
+    bias <- c()
+    for(wa in names(env.data)){
+      if (wa == sel.var[y])
+        next()
+      pred <- predict(fi.out, watchlist[[wa]])
+      bias <- c(bias, mean((pred - env.data[[wa]][,which(subs == y)])))
+    
+      # qqplot(dat[,y], env.data[[wa]][,which(subs == y)])
+      # abline(0,1)
+      # qqplot(dat[,which(sel.var == wa)], env.data[[sel.var[y]]][,which(sel.var[subs] == wa)])
+      # abline(0,1)
+      plot(dat[,which(sel.var == wa)], dat[,y], xlim = c(0, 6), ylim = c(0, 6),
+           xlab = wa, ylab = sel.var[y])
+      points(env.data[[wa]][,which(sel.var[subs] == wa)],  env.data[[wa]][,which(subs == y)], col = 2)
+      # points(env.data[[sel.var[y]]][,which(sel.var[subs] == wa)],  env.data[[sel.var[y]]][,which(subs == y)], col = 3)
+      points(env.data[[wa]][,which(sel.var[subs] == wa)], pred, col = 3)
+    }
+    
+
+    out <- cbind(pval, nsplit, abs(bias))
+    rownames(out) <- paste("$X_{", subs[subs != y], "}$", sep = "")
+    cat("\\multirow{", nrow(out), "}{*}{$X_{", y, "}$}", sep ="")
+    for(i in 1:nrow(out)){
+      cat("& ", rownames(out)[i], " & ", paste(format(out[i,], digits = 2), collapse = " & "), " \\", "\\", "\n", sep = "")
+    }
+  }
 }
-
