b107b0d08def56c3c68f6b90915b30e6e48a39ce
Christoph Schultheiss <christoph.schultheiss@stat.math.ethz.ch>
2024-04-03 10:47:42 +0200

    Colummn selection for allfit

--- fitpred.R
+++ fitpred.R
@@ -8,12 +8,12 @@ predxg <- function(fit, data, ind){
   fit$pred[ind]
 }
 
-allfitxg <- function(data, max_depth = 2, verbose = FALSE){
-  data.x <- xgb.DMatrix(as.matrix(data[,-1]), label = data[,1])
+allfitxg <- function(data, res.ind = 1, max_depth = 2, verbose = FALSE){
+  data.x <- xgb.DMatrix(as.matrix(data[,-res.ind]), label = data[,res.ind])
   fi.all <- xgb.cv(list(max_depth = max_depth, nthread = 1), data = data.x, nrounds = 500, nfold = 2, early_stopping_rounds = 3, prediction = TRUE, verbose = verbose)
   out <- list()
   out$fitted.values <- fi.all$pred
-  out$residuals <- data$y - fi.all$pred
+  out$residuals <- data[, res.ind] - fi.all$pred
   out
 }
 
@@ -35,14 +35,14 @@ normxg.het <- function(fit, data, ind){
   sqrt(fit[[2]]$pred[ind] - fit[[1]]$pred[ind]^2)
 }
 
-allfitxg.het <- function(data){
-  data.x <- xgb.DMatrix(as.matrix(data[,-1]), label = data[,1])
-  fi.all <- xgb.cv(list(max_depth = 2, nthread = 1), data = data.x, nrounds = 500, nfold = 2, early_stopping_rounds = 3, prediction = TRUE, verbose = F)
-  data.x2 <- xgb.DMatrix(as.matrix(data[,-1]), label = data[,1]^2)
-  fi.all2 <- xgb.cv(list(max_depth = 2, nthread = 1), data = data.x2, nrounds = 500, nfold = 2, early_stopping_rounds = 3, prediction = TRUE, verbose = F)
+allfitxg.het <- function(data, res.ind = 1, max_depth = 2, verbose = FALSE){
+  data.x <- xgb.DMatrix(as.matrix(data[,-res.ind]), label = data[,res.ind])
+  fi.all <- xgb.cv(list(max_depth = max_depth, nthread = 1), data = data.x, nrounds = 500, nfold = 2, early_stopping_rounds = 3, prediction = TRUE, verbose = verbose)
+  data.x2 <- xgb.DMatrix(as.matrix(data[,-res.ind]), label = data[,res.ind]^2)
+  fi.all2 <- xgb.cv(list(max_depth = max_depth, nthread = 1), data = data.x2, nrounds = 500, nfold = 2, early_stopping_rounds = 3, prediction = TRUE, verbose = verbose)
   out <- list()
   out$fitted.values <- fi.all$pred
-  resu <- data$y - fi.all$pred
+  resu <- data[, res.ind] - fi.all$pred
   norm <- sqrt(fi.all2$pred - fi.all$pred^2)
   out$residuals <- resu/norm
   if (any(is.na(out$residuals)))
